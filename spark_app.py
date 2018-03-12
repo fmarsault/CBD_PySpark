4e0284b27859665b2c0ab59d128b8517bc8902e8
OursDataniere <felix.marsault@gmail.com>
2018-03-12 19:03:33 +0100

    change show to collect

--- spark_app.py
+++ spark_app.py
@@ -54,10 +54,10 @@ def process_rdd(time, rdd):
             print("Error4: %s" % e)
         try:
             # get the top 10 hashtags from the table using SQL and print them
-            # f = udf(lambda x: x.encode('utf-8'), StringType())
+            f = udf(lambda x: x.encode('utf-8'), StringType())
             # hashtag_counts_df = sql_context.sql(
             #     "select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10".format(hashtags_df))
-            hashtag_counts_df = sql_context.table("hashtags").select('hashtag')
+            hashtag_counts_df = sql_context.table("hashtags").select(f('hashtag'))
             hashtag_counts_df.show()
             for x in hashtag_counts_df.collect():
                 print(x)

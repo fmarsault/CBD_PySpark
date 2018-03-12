13ef8947a95442aa9d0594d53fc64a943958e119
fmarsault <felix.marsault@gmail.com>
2018-03-12 12:34:33 +0100

    errors

--- spark_app.py
+++ spark_app.py
@@ -25,23 +25,47 @@ def get_sql_context_instance(spark_context):
 def process_rdd(time, rdd):
     print("----------- %s -----------" % str(time))
     try:
-        # Get spark sql singleton context from the current context
-        sql_context = get_sql_context_instance(rdd.context)
-        # convert the RDD to Row RDD
-        row_rdd = rdd.map(lambda w: Row(hashtag=w[0], hashtag_count=w[1]))
-        # create a DF from the Row RDD
-        hashtags_df = sql_context.createDataFrame(row_rdd)
-        # Register the dataframe as table
-        hashtags_df.registerTempTable("hashtags")
-        # get the top 10 hashtags from the table using SQL and print them
-        hashtag_counts_df = sql_context.sql(
-            "select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10")
-        hashtag_counts_df.show()
-        # call this method to prepare top 10 hashtags DF and send them
-        send_df_to_dashboard(hashtag_counts_df)
+        try:
+            # Get spark sql singleton context from the current context
+            sql_context = get_sql_context_instance(rdd.context)
+        except:
+            e = sys.exc_info()[0]
+            print("Error1: %s" % e)
+        try:
+            # convert the RDD to Row RDD
+            row_rdd = rdd.map(lambda w: Row(hashtag=w[0], hashtag_count=w[1]))
+        except:
+            e = sys.exc_info()[0]
+            print("Error2: %s" % e)
+        try:
+            # create a DF from the Row RDD
+            hashtags_df = sql_context.createDataFrame(row_rdd)
+        except:
+            e = sys.exc_info()[0]
+            print("Error3: %s" % e)
+        try:
+            # Register the dataframe as table
+            hashtags_df.registerTempTable("hashtags")
+        except:
+            e = sys.exc_info()[0]
+            print("Error4: %s" % e)
+        try:
+            # get the top 10 hashtags from the table using SQL and print them
+            hashtag_counts_df = sql_context.sql(
+                "select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10")
+            hashtag_counts_df.show()
+        except:
+            e = sys.exc_info()[0]
+            print("Error5: %s" % e)
+        try:
+            # call this method to prepare top 10 hashtags DF and send them
+            send_df_to_dashboard(hashtag_counts_df)
+        except:
+            e = sys.exc_info()[0]
+            print("Error6: %s" % e)
     except:
         e = sys.exc_info()[0]
-        print("Error: %s" % e)
+        print("ErrorAll: %s" % e)
 
 
 def send_df_to_dashboard(df):
@@ -65,7 +89,7 @@ sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 4)
 # setting a checkpoint to allow RDD recovery
 ssc.checkpoint("checkpoint_MastodonApp")
-# read data from port 4444
+# read data from port 9009
 dataStream = ssc.socketTextStream("localhost", 9009)
 
 # split each tweet into words
